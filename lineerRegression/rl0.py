75c6be6631cc0914e1544a44f8aaac12c67de801
alperenkara <alprnkr@gmail.com>
2018-08-23 13:41:43 +0200

    2308

--- lineerRegression/rl0.py
+++ lineerRegression/rl0.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt 
+filePath= "linear-regression-dataset.csv"
 
-df = pd.read_csv("linear-regression-dataset.csv")
+df = pd.read_csv(filePath,sep=";")
 
-df2 = pd.DataFrame(df,columns = ['deneyim','maas'])
-
-print(df2)
+plt.scatter(df.deneyim, df.maas)
+plt.xlabel('deneyim')
+plt.ylabel('maas')
+plt.show()
