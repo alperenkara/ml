420b24271e1f50e708cf6ea1032bb445bf1f8b79
PLALKAR1 <alperen.kara@pl.abb.com>
2018-10-01 11:02:09 +0200

    rsquare

---
+++ evaluationRegModels/rsquare.py
@@ -0,0 +1,20 @@
+# performance model
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv(r"C:\Users\PLALKAR1\Documents\GitHub\ml\evaluationRegModels\randomForestReg.csv",sep=";",header=None)
+
+x = df.iloc[:,0].values.reshape(-1,1)
+y = df.iloc[:,1].values.reshape(-1,1)
+
+from sklearn.ensemble import RandomForestRegressor
+
+rf = RandomForestRegressor(n_estimators=100, random_state= 42)
+rf.fit(x,y)
+
+y_head = rf.predict(x)
+
+from sklearn.metrics import r2_score
+
+print('r_score: {}'.format(r2_score(y,y_head)))
