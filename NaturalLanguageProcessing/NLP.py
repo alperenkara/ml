394c671a4b3ea14319937b8b6a78cf16bbb145bc
Alperen Kara <alperen.kara@pl.abb.com>
2019-01-02 14:59:33 +0100

    NLP01

--- NaturalLanguageProcessing/NLP.py
+++ NaturalLanguageProcessing/NLP.py
@@ -11,3 +11,6 @@ import sys
 
 data = pd.read_csv(r"D:\mygit\ml\NaturalLanguageProcessing\gender-classifier.csv",encoding = "latin1")
 # data = pd.read_csv("./gender-classifier.csv",encoding = "latin1")
+data = pd.concat([data.gender,data.description],axis=1)
+data.dropna(axis = 0, inplace = True)
+data.gender = [1 if each == "female" else 0 for each in data.gender] 
