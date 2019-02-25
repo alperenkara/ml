ddb1b7eb02f0ee36292c3a083b97a10179baa2bc
Alperen Kara <alperen.kara@pl.abb.com>
2019-02-25 10:03:31 +0100

    bags of words

--- NaturalLanguageProcessing/nlp_v2.py
+++ NaturalLanguageProcessing/nlp_v2.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jul 14 13:13:34 2018
 
-@author: kcy
 """
 import pandas as pd
 
-# %% import twitter data
-data = pd.read_csv(r"gender_classifier.csv",encoding = "latin1")
+#  import twitter data
+data = pd.read_csv(r"D:\\mygit\\ml\\NaturalLanguageProcessing\gender-classifier.csv",encoding = "latin1")
 data = pd.concat([data.gender,data.description],axis=1)
 data.dropna(axis = 0,inplace = True)
 data.gender = [1 if each == "female" else 0 for each in data.gender]
 
-#%% clening data 
+# clening data 
 # regular expression RE mesela "[^a-zA-Z]"
 import re
 
@@ -20,7 +18,7 @@ first_description = data.description[4]
 description = re.sub("[^a-zA-Z]"," ",first_description)  # a dan z ye ve A dan Z ye kadar olan harfleri bulma geri kalanları " " (space) ile degistir
 description = description.lower()   # buyuk harftan kucuk harfe cevirme
 
-# %% stopwords (irrelavent words) gereksiz kelimeler
+#  stopwords (irrelavent words) gereksiz kelimeler
 import nltk # natural language tool kit
 nltk.download("stopwords")      # corpus diye bir kalsore indiriliyor
 from nltk.corpus import stopwords  # sonra ben corpus klasorunden import ediyorum
@@ -31,21 +29,21 @@ from nltk.corpus import stopwords  # sonra ben corpus klasorunden import ediyoru
 description = nltk.word_tokenize(description)
 
 # split kullanırsak "shouldn't " gibi kelimeler "should" ve "not" diye ikiye ayrılmaz ama word_tokenize() kullanirsak ayrilir
-# %%
+
 # greksiz kelimeleri cikar
 description = [ word for word in description if not word in set(stopwords.words("english"))]
   
-# %%             
+             
 # lemmatazation loved => love   gitmeyecegim = > git
 
 import nltk as nlp
-
+nltk.download("wordnet")
 lemma = nlp.WordNetLemmatizer()
 description = [ lemma.lemmatize(word) for word in description] 
 
 description = " ".join(description)
 
-#%% 
+
 description_list = []
 for description in data.description:
     description = re.sub("[^a-zA-Z]"," ",description)
@@ -57,7 +55,7 @@ for description in data.description:
     description = " ".join(description)
     description_list.append(description)
 
-# %% bag of words
+
 
 from sklearn.feature_extraction.text import CountVectorizer # bag of words yaratmak icin kullandigim metot
 max_features = 5000
@@ -68,7 +66,7 @@ sparce_matrix = count_vectorizer.fit_transform(description_list).toarray()  # x
 
 print("en sik kullanilan {} kelimeler: {}".format(max_features,count_vectorizer.get_feature_names()))
 
-# %%
+# 
 y = data.iloc[:,0].values   # male or female classes
 x = sparce_matrix
 # train test split
@@ -76,12 +74,12 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.1, random_state = 42)
 
 
-# %% naive bayes
+#  naive bayes
 from sklearn.naive_bayes import GaussianNB
 nb = GaussianNB()
 nb.fit(x_train,y_train)
 
-#%% prediction
+# prediction
 y_pred = nb.predict(x_test)
 
 print("accuracy: ",nb.score(y_pred.reshape(-1,1),y_test))
